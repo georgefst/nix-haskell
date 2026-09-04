18d01b21fd2f0a0c9433af2286b1aca5031de6d7
George Thomas <georgefsthomas@gmail.com>
2026-09-04 20:44:58 +0100

    revert previous commit...
    including, crucially, Haskell.nix submodule
    
    this was somehow (via the resulting Nixpkgs bump, probably) breaking the prototype Obelisk TUI - I haven't looked in to why, but it's something to do with either file watching or the `readyCmd`
    
    it also corrupts the postgres DB used by Gargoyle, though that can be fixed with `rm db`

--- modules/haskell.nix/default.nix
+++ modules/haskell.nix/default.nix
@@ -422,10 +422,13 @@ in {
         '';
       };
 
+      # HLS 2.14 requires significant patches to work with GHC 9.14, particularly for the Ormolu+Fourmolu plugins.
+      # So we build a more recent version from source instead, from the `master` branch.
+      # We should be able to just bump this to "2.15" once that's released (expected early-mid September).
       haskell-nix.options.shell.tools.haskell-language-server = mkDefault {
         src = builtins.fetchGit {
           url = "https://github.com/haskell/haskell-language-server.git";
-          rev = "1b4b3c6bdd2bf8d1e1182e2e770f5dea9198db80";
+          rev = "16bf0466293daabac6f216d2af7e5bccbb91fcc1";
         };
         cabalProjectLocal = ''
           package *
